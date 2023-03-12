3b7cefdec99acd751363be97f05b9e3c3c4a0094
Charles Davis <creepervenom10@gmail.com>
2023-03-11 17:25:07 -0700

    Registration Page - No Existing User Check
    New Features:
    + User Registration Page Form
    + Insert User from registration page into MongoDB

---
+++ Controllers/RegistrationController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskNinja.Models;
+using TaskNinja.Services;
+
+namespace TaskNinja.Controllers
+{
+    /// <summary>
+    /// Class <c>RegistrationController</c> contains the endpoints for regestering new users
+    /// <br></br>
+    /// Author(s): Charles Davis
+    /// <br></br>
+    /// Version: 0.1
+    /// </summary>
+    /// <seealso cref="Microsoft.AspNetCore.Mvc.Controller" />
+    public class RegistrationController : Controller
+    {
+        /// <summary>
+        /// The users DAO
+        /// </summary>
+        private IUsersDAO usersDAO;
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RegistrationController"/> class.
+        /// </summary>
+        public RegistrationController()
+        {
+            this.usersDAO = new UsersDAO();
+        }
+
+        /// <summary>
+        /// GET: /Registration
+        /// <br></br>
+        /// Author(s): Charles Davis
+        /// </summary>
+        /// <returns>Registration View</returns>
+        public IActionResult Index()
+        {
+            // Create user model to send to registration page
+            UserModel user = new UserModel();
+            // Send user model and view to page
+            return View(user);
+        }
+        /// <summary>
+        /// Processes the registration using UserDAO to register a new user into DB.
+        /// <br></br>
+        /// Author(s): Charles Davis
+        /// </summary>
+        /// <param name="user">The user to be registered.</param>
+        /// <returns>Response page</returns>
+        [HttpPost]
+        public async Task<IActionResult> ProcessRegistration(UserModel user)
+        {
+            // Checks to see if model is valid
+            if(ModelState.IsValid)
+            {
+                // Wait for registration to complete
+                if(await usersDAO.RegisterUser(user) == UsersDAO.msgSUCCESS) 
+                {
+                    ViewBag.message = "Registration Success!";
+                    // Send usermodel to registration success view
+                    return View("Index", user);
+                }
+                // If registration failed
+                else
+                {
+                    ViewBag.message = "Registration Failed!";
+                    // Send usermodel back to registration view
+                    return View("Index", user);
+                }
+            }
+            // If model is not valid
+            else
+            {
+                ViewBag.message = "Registration Failed: Invald Model!";
+                // Send usermodel back to registration view
+                return View("Index", user);
+            }
+        }
+    }
+}
--- Services/UsersDAO.cs
+++ Services/UsersDAO.cs
@@ -9,27 +9,31 @@ namespace TaskNinja.Services
     /// <summary>
     /// Class <c>UsersDAO</c> provides a service for getting, creating, and authenticating users with the MongoDB database.
     /// <br></br>
-    /// Author(s): Gabriel Cepleanu
+    /// Author(s): Gabriel Cepleanu, Charles Davis
     /// <br></br>
     /// Version: 0.1
     /// </summary>
 	public class UsersDAO : IUsersDAO
 	{
+        // Task<String> Responses
+        public const String msgSUCCESS = "Success";
+        public const String msgFAILURE = "Failure";
 
         // INSERT MONGODB CONNECTION STRING HERE
-        private string connectionString = "";
+        private const string connectionString = "";
         private MongoClient mongoClient;
         private IMongoDatabase database;
         private IMongoCollection<BsonDocument> collection;
-        
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UsersDAO"/> class.
+        /// </summary>
         public UsersDAO()
 		{
             this.mongoClient = new MongoClient(connectionString);
             this.database = mongoClient.GetDatabase("Task-Ninja");
             this.collection = database.GetCollection<BsonDocument>("User");
         }
-
         public Task<UserModel> GetUserByID(string id)
         {
             throw new NotImplementedException();
@@ -56,9 +60,21 @@ namespace TaskNinja.Services
             return documents.FirstOrDefault() != null;
         }
 
-        public Task<string> RegisterUser(UserModel user)
+        public async Task<string> RegisterUser(UserModel user)
         {
-            throw new NotImplementedException();
+            // TODO: Check for existing username/email
+            // Converts UserModel to BsonDocument
+            var document = new BsonDocument
+            {
+                {"firstName",  user.firstName},
+                {"lastName", user.lastName },
+                {"email", user.email},
+                {"username", user.username},
+                {"password", user.password}
+            };
+            // Inserts user data BsonDocuemtn into table
+            await this.collection.InsertOneAsync(document);
+            return msgSUCCESS;
         }
     }
 }
